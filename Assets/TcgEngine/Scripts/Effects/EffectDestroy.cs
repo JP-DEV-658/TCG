05ee8ee1ca400e6cd829fee09a3438b3e65d249f
JP-DEV-658 <jpdev65865.git@gmail.com>
2024-07-13 08:35:21 +0100

    v1

---
+++ Assets/TcgEngine/Scripts/Effects/EffectDestroy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TcgEngine.Gameplay;
+
+namespace TcgEngine
+{
+    [CreateAssetMenu(fileName = "effect", menuName = "TcgEngine/Effect/Destroy", order = 10)]
+    public class EffectDestroy : EffectData
+    {
+        public override void DoEffect(GameLogic logic, AbilityData ability, Card caster, Card target)
+        {
+            logic.KillCard(caster, target);
+        }
+
+    }
+}
